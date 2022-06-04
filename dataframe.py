0ac2010c37e879001912aaa024e83884727abc91
jl-tavera <jl.tavera@uniandes.edu.co>
2022-06-04 03:16:16 -0500

    new Teams

--- dataframe.py
+++ dataframe.py
@@ -62,5 +62,5 @@ def getSigningsData(col_name, name):
 # data = (Dfx.completeRawData('signings.csv'))
 # FBref.exportFinalCSV(data,'','data')
  
-df_ML = Dfx.createRFDF('data.csv', 'Santa Fe')
+df_ML = Dfx.createRFDF('data.csv', 'Jaguares')
 
